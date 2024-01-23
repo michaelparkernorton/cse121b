06129148a73231c7ea8595160dd7692f261eb980
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-23 09:25:59 -0300

    updates

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,39 +1,68 @@
 /* W05: Programming Tasks */
 
 /* Step 3: Declare and initialize global variables */
-const templesElement = document.getElementById("temples")
-let templeList = []
+const templesElement = document.getElementById("temples");
+let templeList = [];
 
 /* Step 4: async displayTemples Function */
-const displayTemples = (temples) => temples.forEach(temple => {
-	const article = document.createElement("article")
-	const h3 = document.createElement("h3")
-	h3.textContent = temple.templeName
-	const img = document.createElement("img")
-	img.src = temple.imageUrl
-	img.alt = temple.location
-	article.appendChild(h3)
-	article.appendChild(img)
-	templesElement.appendChild(article)
-});
-
+const displayTemples = (temples) =>
+  temples.forEach((temple) => {
+    const article = document.createElement("article");
+    const h3 = document.createElement("h3");
+    h3.textContent = temple.templeName;
+    const img = document.createElement("img");
+    img.src = temple.imageUrl;
+    img.alt = temple.location;
+    article.appendChild(h3);
+    article.appendChild(img);
+    templesElement.appendChild(article);
+  });
 
 /* Step 5 async getTemples Function using fetch()*/
 const getTemples = async () => {
-	const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json")
-	if (response.ok) {
-		templeList = await response.json();
-	}
-	displayTemples(templeList);
-	// console.log(templeList);
-}
+  const response = await fetch(
+    "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json"
+  );
+  if (response.ok) {
+    templeList = await response.json();
+  }
+  displayTemples(templeList);
+};
 
-/* reset Function */
+/* Step 6 reset Function */
+const reset = () => templesElement.replaceChildren();
 
-
-/* filterTemples Function */
-
+/* Step 7 filterTemples Function */
+const filterTemples = (temples) => {
+  reset();
+  const filter = document.getElementById("filtered");
+  switch (filter.value) {
+    case "utah":
+      displayTemples(
+        temples.filter((temple) => temple.location.includes("Utah"))
+      );
+      break;
+    case "notutah":
+      displayTemples(
+        temples.filter((temple) => !temple.location.includes("Utah"))
+      );
+      break;
+    case "older":
+      displayTemples(
+        temples.filter((temple) => new Date(temple.dedicated) < new Date(1950, 0, 1))
+      );
+      break;
+    case "all":
+      displayTemples(temples)
+      break;
+    default:
+    // code block
+  }
+};
 
 getTemples();
 
-/* Event Listener */
+/* Step 8 Event Listener */
+document.getElementById("filtered").addEventListener("change", () => {
+  filterTemples(templeList);
+});
