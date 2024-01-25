ddc166f8a9e5c7511c9154e70458c5fd6594d33f
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-25 18:35:31 -0300

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,26 @@ const toggleMenu = () => {
     document.querySelector('#menu').classList.toggle('open');
 }
 
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
+document.querySelector('#toggleMenu').addEventListener('click', toggleMenu)
 
+
+// function getCurrentURL () {
+//     return window.location.href
+//   }
+  
+//   // Example
+//   const url = getCurrentURL()
+  
+// console.log(url);
+
+
+// if (url.includes('index')) {
+//     console.log('active');
+//     setTimeout(console.log(document.getElementById('index')), 3000)
+//     setTimeout(document.getElementById('index').classList.add('active'), 3000)
+// }
+// else if (url.includes('w01')) {
+//     console.log('active');
+//     console.log(document.getElementById('w01'));
+//     document.getElementById('w01').classList.add('active');
+// }
