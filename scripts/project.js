454b99d5954c5bc3e74a41ebc6120d48b05fdafb
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-29 19:32:34 -0300

    update

--- scripts/project.js
+++ scripts/project.js
@@ -1,6 +1,7 @@
 const link = `https://script.googleusercontent.com/macros/echo?user_content_key=TWRoq9tE8EXS4tX5PgL6dJA0sEIyCKnkKBuBZag4eZSBo_G8kzzG31c9xiSdIug3yTqjrC7nwcN6k9ovPc9Di8Q_P8CEebOIm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnI8WeWdA16be5QIqVNbb26IKk80DMBdupXDlhajCIhQFdMnZ29P8ayTIqKk2EvT3LaMWFLbeABmCBpYTnYf13Ef8Eqx3Gnv6Yg&lib=MhWEx1VRtJCjkT0F-E2LvlEngEDx33c-r`;
 
-const link2 = "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json"
+//THE SHS SCHEDULE PAGE
+const link2 = "https://strasburghs.rschoolteams.com/page/2981"
 /* Step 3: Declare and initialize global variables */
 const gamesElement = document.getElementById("games");
 let gameList = [];
