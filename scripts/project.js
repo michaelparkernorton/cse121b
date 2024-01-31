e54ced1478da7a0309c6f6296a8caa6fc93e870a
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-31 11:34:32 -0300

    updates

--- scripts/project.js
+++ scripts/project.js
@@ -5,13 +5,31 @@ const link2 = "https://strasburghs.rschoolteams.com/page/2981"
 const link3 = "../json/project.json"
 const link4 = "../json/previousSeason.json"
 /* Step 3: Declare and initialize global variables */
-const gamesElement = document.getElementById("games");
-const template = document.querySelector("[data-template]");
-let gameList = [];
+const gamesElement = document.getElementById("games")
+const template = document.querySelector("[data-template]")
+let gameList = []
+const goalsFor = []
+const goalsAgainst = []
+const scores = []
+const wins = []
+const losses = []
+
+function convertWinsLosses(result, type) {
+  if (result == type) {
+    return 1
+  }
+  else {
+    return 0
+  }
+}
 
 /* Step 4: async displayGames Function */
 const displayGames = (games) =>
   games.forEach((game) => {
+    goalsFor.push(parseInt(game.Score.slice(0,1)))
+    goalsAgainst.push(parseInt(game.Score.slice(-1)))
+    wins.push(convertWinsLosses(game['W/L'], 'W'))
+    losses.push(convertWinsLosses(game['W/L'], 'L'))
     const card = template.content.cloneNode(true).children[0]
     const cardDate = card.querySelector("[data-date]")
     const cardTime = card.querySelector("[data-time]")
@@ -40,12 +58,41 @@ const getGames = async () => {
   }
   // const tr = document.createElement("tr")
   displayGames(gameList.data)
+  console.log(goalsFor);
+  console.log(goalsAgainst);
+  const initialValue = 0;
+  const goalsForTotal = goalsFor.reduce((accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+  const goalsAgainstTotal = goalsAgainst.reduce((accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+  const winsTotal = wins.reduce((accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+  const lossesTotal = losses.reduce((accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+  console.log(goalsForTotal);
+  console.log(goalsAgainstTotal);
+  console.log(winsTotal);
+  console.log(lossesTotal);
+
+  const card = template.content.cloneNode(true).children[0]
+  const cardDate = card.querySelector("[data-date]")
+  const cardTime = card.querySelector("[data-time]")
+  const cardOpponent = card.querySelector("[data-opponent]")
+  const cardLocation = card.querySelector("[data-location]")
+  const cardScore = card.querySelector("[data-score]")
+  const cardWL = card.querySelector("[data-w-l]")
+  cardDate.textContent = ""
+  cardTime.textContent = ""
+  cardOpponent.textContent = ""
+  cardLocation.textContent = ""
+  cardScore.textContent = `${goalsForTotal} - ${goalsAgainstTotal}`
+  cardWL.textContent = `${winsTotal} - ${lossesTotal}`
+
+  gamesElement.append(card);
 };
 
 getGames();
-
-
-// ADDITIONAL REQUIREMENTS
-// Include conditional branching.
-// Use an array method.
-// Exclusively use template literals when building strings.
